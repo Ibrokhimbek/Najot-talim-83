fcf4633538a9ea136b5a64c4fb2c60c982d355b8
Ibrokhimbek <ibrohim1348@gmail.com>
2022-11-05 21:07:58 +0500

    3-2 intro to JS

---
+++ 3-month/Lesson 2/js/main.js
@@ -0,0 +1,73 @@
+// var Ibrohim = 3
+// var firstname = "Ibrohim"
+// console.log(Ibrohim)
+
+// var x = 5;
+// var y = 7;
+// var z = x + y;
+// console.log(z);
+
+//* Data types =>
+//* 1.Primitive vs 2.Non-primitive
+
+//? 1.String
+// var firstName = "Ibrohim";
+// var firstNameTwo = 'Abror';
+// var firstNameThree = `5`;
+// var aralash = 'o\'quvchi';
+// var gap = 'Hokim kelib "Silarga pul beraman" deb ketti';
+// var backticks = `${firstNameTwo} kelib "Silarga pul beraman" deb ketti`;
+// console.log(backticks);
+// console.log(typeof firstName);
+// console.log(typeof firstName);
+// console.log(typeof firstNameTwo);
+// console.log(typeof firstNameThree);
+
+//? 2.Number
+// var num = 566;
+// var num2 = "566";
+// console.log(typeof num);
+// console.log(typeof num2);
+
+//? 3.Boolean
+// var isOpen = false; // can be switched into true
+// console.log(typeof isOpen);
+
+//? 4.Undefined
+// console.log(typeof hello)
+
+//? 5.Null
+// var username = null;
+// console.log(typeof username);
+
+//! Alert
+// alert("Hello Uzbekistan")
+
+//! Prompt
+// var data = prompt("Ma'lumot kiriting:");
+
+//! Amaliyot
+// var birthYear = prompt("Tug'ilgan yilingizni kiriting:")
+// var year = birthYear;
+// console.log(typeof year);
+// var result = 2022 - birthYear;
+// console.log(result);
+
+//! deep in var
+// var x, y, z;
+// console.log(x, y, z);
+// x = 5;
+// y = 6;
+// z = 10;
+// console.log(x, y, z);
+
+// x = 10;
+// var x = 5;
+// username = "Abror";
+// console.log(x);
+// console.log(username);
+
+// var x = 5;
+// console.log(x);
+// x=10;
+// console.log(x);
