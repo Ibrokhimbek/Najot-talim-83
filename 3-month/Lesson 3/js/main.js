be8545052d415c7fa8cbb171e370776d186ab89e
Ibrokhimbek <ibrohim1348@gmail.com>
2022-11-08 20:52:47 +0500

    js

---
+++ 3-month/Lesson 3/js/main.js
@@ -0,0 +1,81 @@
+//? taqqoslash / boolean valuelar
+// var x = 10;
+// var y = 4;
+// console.log(x%y);
+// console.log(2**2);
+// console.log(2<3) // true
+// console.log(2>3) // false
+// console.log(2>=2) // true
+// console.log(2<=2) // true
+// console.log(2<=3) // true
+// console.log(3>=3) // true
+// console.log(2==2) // true
+// console.log(3==2) // false
+// console.log(2=="2") // true
+// console.log(2==="2") // false
+// console.log("Olma" === "Olma")
+// console.log(true > false)
+// console.log(true < false)
+// console.log(true == 1) // true
+// console.log(true === 1) // false
+// console.log(true+true) // 2
+//? if else
+// var usernameInput = prompt("Enter your username:");
+// var passwordInput = prompt("Enter your password:");
+
+// var username = "abror.boy";
+// var password = "123987";
+
+// console.log(usernameInput === username);
+// console.log(passwordInput === password);
+
+// if (usernameInput === username) {
+//   if (passwordInput === password) {
+//     console.log("Welcome to Home!");
+//   } else {
+//     console.log("Your password is not correct!");
+//   }
+// } else {
+//   console.log("Your username is not correct!");
+// }
+
+//? else if
+// var username = prompt("Username:");
+
+// if (username === "Abror") {
+//   alert("Abror");
+// } else if (username === "Ismoil") {
+//   alert("Ismoil");
+// } else if (username === "Komil") {
+//   alert("Komil");
+// } else {
+//   alert("Username to'g'ri kelmadi");
+// }
+
+//? querySelector()
+// var value = prompt("Type your value");
+
+// var headingEl = document.querySelector("#heading");
+
+// if (value !== "") {
+//   headingEl.textContent = value;
+// }
+
+// console.log(2 !== 2); // false
+// console.log(2 !== 3); // true
+
+// Xarajatlar hisoblagichi.
+
+// Alisher chet elga dam olishga bormoqchi. Safar uchun ketadigan xarajatni internetdagi saytlardan qidirib chiqib, quyidagi ro’yxatda berilgan ma’lumotlarni topdi. Ularning ba’zilari AQSh dollarida ko’rsatilgan bo’lsa, ba’zilari yevroda berilgan.
+
+// - Borish-kelish samolyot bileti - $500
+// - Mehmonxona to’lovi (to’liq safar davomiyligi uchun) - $250
+// - Muzey va ko’ngilochar joylar uchun - 120 yevro
+
+// $1 = 10650.34 so’m
+// 1 yevro = 10650.03 so’m
+
+// Tuzilishi kerak bo’lgan dastur quyidagi ko’rinishda ishlaydi:
+// 1. Alisher o’zida necha pul bor ekanligini so’mda prompt orqali ko’rsatadi.
+// 2. Xarajatlar dollar va yevrodan so’mga o’tkaziladi.
+// Consoleda xarajat bilan umumiy o'zining summasini chiqarib bersin
