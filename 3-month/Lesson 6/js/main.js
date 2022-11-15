964feaa1c4c9c9c2bc1eba1c496c2c4ac660bac9
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-11-15 21:24:07 +0500

    error types, functions, for loop

---
+++ 3-month/Lesson 6/js/main.js
@@ -0,0 +1,135 @@
+//? Functions
+//! 1. Function declaration
+// function sayHello1(str) {
+//   console.log(str);
+// }
+// sayHello1("Hello One");
+
+//! 2. Function expression
+// const sayHello2 = function (str) {
+//   console.log(str);
+// };
+// sayHello2("Hello Two");
+
+//! 3. Arrow function
+// const sayHello3 = (str = "Salam", second = 5) => {
+//   console.log(str, second);
+// };
+// sayHello3();
+
+//* Amaliyot
+// function hello(firstname = false) {
+//   if (firstname) {
+//     return `Assalamu alaikum ${firstname}!`;
+//   } else {
+//     return "Assalamu alaikum hurmatli mijoz!";
+//   }
+// }
+// console.log(hello());
+// console.log(hello("Ibrohim"));
+
+//? SyntaxError, ReferenceError, TypeError
+//! SyntaxError
+// function bye()
+//   console.log("Bye")
+// }
+
+//! ReferenceError
+// console.log(c)
+
+//! TypeError
+// const num = "Hlfadlsfj"
+// console.log(num.reverse(5))
+
+//? Arrays
+// const user = {
+//   name: "Userbek"
+// }
+// console.log(user["name"].length);
+
+// const users = ["Ibrohim", "Ravshan", "Ozodbek", "Ismoil", "Abror", "Aziz"];
+// users[users.length] = "Userbek";
+// users[users.length] = "Mishkabek";
+// console.log(users);
+
+// users[8] = "Userbek"
+// console.log(users[7]);
+// console.log(users["length"]);
+// console.log(users.length);
+
+//? Array methods
+// const users = ["Ibrohim", "Ravshan", "Ozodbek", "Ismoil", "Abror", "Aziz"];
+//! push => array oxiridan ma'lumot qo'shadi
+// users.push("Abdulla");
+// console.log(users.push("Sardorbek")); // array length => 7
+// console.log(users);
+//! pop => array oxiridan bitta ma'lumot olib tashlaydi
+// console.log(users.pop()); // Aziz
+// console.log(users);
+
+//! unshift => array boshidan ma'lumot qo'shadi
+// console.log(users);
+// console.log(users.unshift("Hello")); // array length => 7
+// console.log(users);
+
+//! shift => array boshidan bitta ma'lumot olib tashlaydi
+// console.log(users);
+// console.log(users.shift()); // Ibrohim
+// console.log(users);
+
+// const users = ["Ibrohim", "Ravshan", "Ozodbek", "Ismoil", "Abror", "Aziz"];
+// console.log(users.indexOf("Ravshan"));
+// console.log(users.includes("Abror"))
+
+//* Amaliyot
+const users = [];
+
+function add() {
+  const firstname = prompt("Firstname:");
+  const age = prompt("Age:");
+
+  const userObject = {
+    name: firstname,
+    age: age,
+  };
+
+  users.push(userObject);
+  console.log(users);
+}
+
+function check() {
+  const firstname = prompt("Firstname to check:");
+  const age = prompt("Age to check:");
+
+  const checkObj = {
+    name: firstname,
+    age: age,
+  };
+
+  let isStudent = false;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name === checkObj.name && users[i].age === checkObj.age) {
+      isStudent = true;
+    }
+  }
+
+  if (isStudent) {
+    alert("Welcome to Website!");
+  } else {
+    alert("Your data is incorrect!");
+  }
+}
+
+//* ++ increment
+// let x = 5;
+// x++
+// x++
+// console.log(x);
+
+//? for loop
+// const users = [{name: "Ibrohim", age: 20}, {name: "Ravshan", age: 18}];
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(users[i].name, users[i].age);
+// }
