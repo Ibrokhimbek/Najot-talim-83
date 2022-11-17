d45ad83263bbcc20df0c3f61e6e4303d2366c9cd
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-11-17 20:22:13 +0500

    string and array methods

---
+++ 3-month/Lesson 7/js/main.js
@@ -0,0 +1,44 @@
+function findFive(num) {
+  const numsArr = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 5 === 0 && i % 3 !== 0) {
+      numsArr.push(i);
+    }
+  }
+  return numsArr;
+}
+// console.log(findFive(100));
+
+//? concat, join, slice, splice
+// const arr = findFive(100);
+// console.log(arr);
+// console.log(arr.splice(2,6))
+// console.log(arr);
+// console.log(arr.slice(2, 6))
+// console.log(arr.slice(2, -2))
+// console.log(arr.slice(-6, -2))
+// console.log(arr.slice(2, 3))
+// console.log(arr.join(" / "));
+// console.log(arr.concat(["Ibrohim", "Ismoil"], [1,2,3], {name: "Userbek", age: 99}));
+
+//? String methods
+const username = "Userbek";
+//* String.length
+// console.log(username.length);
+// console.log(username["length"]);
+//* String.trim()
+// const username2 = "         Userbek           ";
+// console.log(username2);
+// console.log(username2.trim());
+//* String.toUpperCase();
+// console.log(username.toUpperCase());
+//* String.toLowerCase();
+// console.log(username.toLowerCase());
+//* String.substring(x,y);
+// console.log(username.substring(0, 4));
+//* String.split();
+// const str = "Ibrohim 20 WIUT Najot-ta'lim";
+// console.log(str);
+// console.log(str.split(" "));
+//* charAt(n)
+console.log(username.charAt(6));
