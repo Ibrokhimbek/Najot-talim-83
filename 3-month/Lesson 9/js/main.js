dda1e89c7edcce9c44e7b5a5ad348b539ce431d8
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-11-22 19:41:00 +0500

    loops from backwards

---
+++ 3-month/Lesson 9/js/main.js
@@ -0,0 +1,29 @@
+//? Nested loops
+// for(let i=0; i<5; i++) {
+//   console.log(i + " --------------------------------------------------")
+//   for(let j=0; j<10; j++) {
+//     console.log(j)
+//   }
+// }
+//! Amaliyot
+// const arr = [
+//   ["Ibrohim", "Userbek", "Vscode", "Javabek", "Sublime jiyan"],
+//   ["Ismoil", "C bobo", "Paskal ajdod", "C# tog'o"],
+//   ["Abdulla", "Playstation aka", "Python kalla"],
+//   ["Jasur", "Asu", "Kamol", "Amir"],
+//   ["Mishkabek", "Klaviaturajon", "Ekranoy"],
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   // console.log(arr[i]);
+//   for (let j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]);
+//   }
+// }
+
+//? Loops from backwards
+// const list = ["Ibrohim", "Userbek", "Vscode", "Javabek", "Sublime jiyan"];
+
+// for (let i = list.length-1; i >= 0; i--) {
+//   console.log(list[i]);
+// }
