30b99cf40b4e8a8962424e1ec01114d7c194cf22
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-11-24 19:49:08 +0500

    Nested loops vs EventListener

--- 3-month/Lesson 10/js/main.js
+++ 3-month/Lesson 10/js/main.js
@@ -6,3 +6,34 @@
 // handler(function () {
 //   console.log("This is coming from callback!")
 // })
+
+//? Nested loops
+
+// let x = 0
+// x++
+// console.log(x);
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+//   for(let j=1; j<4; j++) {
+//     console.log(`${i} => ${j}`)
+//   }
+// }
+
+//? eventListener
+// const obj = {
+//   addEventListener(event, callback) {
+//     if (event === "submit") {
+//       callback({
+//         preventDefault() {
+//           console.log("Prevented");
+//         },
+//       });
+//     }
+//   },
+// };
+
+// obj.addEventListener("submit", (event) => {
+//   event.preventDefault();
+// })
+
