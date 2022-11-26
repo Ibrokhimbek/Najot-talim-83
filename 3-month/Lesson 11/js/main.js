a42de73c79b73d58eb77f0f9416289cfa608b94c
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-11-26 20:53:06 +0500

    form bilan ishlash

---
+++ 3-month/Lesson 11/js/main.js
@@ -0,0 +1,71 @@
+//? for in, for of
+// const obj = {
+//   name: "Ibrohim",
+//   age: 20,
+// };
+
+//* for in
+// const objArr = [];
+
+// for (key in obj) {
+//   const arr = [];
+//   arr.push(key);
+//   arr.push(obj[key]);
+//   objArr.push(arr);
+// }
+
+// console.log(objArr);
+
+//* for of
+// const arr = [1,2,3,4,5]
+
+// for (item of arr) {
+//   console.log(item);
+// }
+
+//* attributes
+// const btnEl = document.querySelector("#btn");
+
+// let num = 0;
+
+// btnEl.addEventListener("click", () => {
+//   if (num < 3) {
+//     btnEl.setAttribute("disabled", "true");
+//     num += 1;
+//     setTimeout(() => {
+//       btnEl.removeAttribute("disabled");
+//     }, 1000);
+//   }
+// });
+
+const myCar = {
+  caption: null,
+  color: null,
+  accessories: [],
+};
+//* console.dir(accessories.children[0].children[0].checked);
+
+const formEl = document.querySelector("#form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const carType = document.getElementsByName("carType");
+  const color = document.querySelector("#color");
+  const accessories = document.querySelectorAll(".accessory__item");
+
+  for (item of carType) {
+    if (item.checked) {
+      myCar.caption = item.value;
+    }
+  }
+
+  myCar.color = color.value;
+
+  for (item of accessories) {
+    if (item.checked) {
+      myCar.accessories.push(item.value);
+    }
+  }
+
+  console.log(myCar);
+});
