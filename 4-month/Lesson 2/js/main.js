4fc0aacec670d492f1a4a687bd787004e5a2220b
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-03 19:38:44 +0500

    Scopes

---
+++ 4-month/Lesson 2/js/main.js
@@ -0,0 +1,15 @@
+//? Scopes => Global, Local or Function scope, Block scope
+
+// const arr = []; //* Global
+
+// function sayHello(item) {
+//   const localScope = "Local scope"; //* Local scope
+//   arr.push(item);
+//   console.log(localScope);
+// }
+
+// if(true) {
+//   const item = "item"; //* Block scope
+// }
+
+// console.log(item);
